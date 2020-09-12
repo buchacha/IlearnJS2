b3ba6ba152861f7ebebc8f4b5f602ef5dac1048c
buchacha <egor322129@gmail.com>
2020-09-12 18:02:39 +0000

    QUESTION SO

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-function filterRange(range, leftBorder, rightBorder) {
-    return range.filter(
-        elem => (leftBorder <= elem && elem <= rightBorder) )
-}
+'use strict'
 
+// function filterRangeInPlace(arr, a, b) {
+
+// }
+
+function filterRangeInPlace(range, leftBorder, rightBorder) {
+    let temp = range.filter(
+        elem => (leftBorder <= elem && elem <= rightBorder) );
+    // range = temp; почему то при присвоении нового массив ведет себя как локальная переменная, а если без этой строчки то влияет на arr
+    range.pop();
+}
 
 let arr = [5, 3, 8, 1];
 
-let filtered = filterRange(arr, 1, 4);
+filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
 
-alert( filtered ); // 3,1 (совпадающие значения)
+alert(arr); // [3, 1]
 
-alert( arr ); // 5,3,8,1 (без изменений)
+// let arr = [1,2,3]
+// let temp = arr;
+// temp.pop();
+// alert(arr);
+
+
