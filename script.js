7bb12d15ec608ac63c90d9dcce680b8fa68ef237
buchacha <egor322129@gmail.com>
2020-09-12 19:21:35 +0000

    object from map

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+'use strict'
+
+let dinner = {buckwheat: 1, sausage:2};
+
 let recipeMap = new Map([
-  ["огурец", 500],
+  [dinner, 500],
   ["помидор", 350],
   ["лук",    50]
 ]);
 
+alert(typeof recipeMap);
 
-for (elem of recipeMap) {
-    alert(elem[0] + ' ' + elem[1] + ' ' + elem[3]);
+let obj = Object.fromEntries(recipeMap);
+
+alert(typeof obj);
+
+objTrue = {param:200};
+
+alert(typeof objTrue);
+
+for (elem in obj) {
+    alert(obj[elem]);
 }
+
+let objTrue2 = {
+    param: 300,
+    {buckwheat:1, sausage:2}: 125 //unexpected token
+};
+
+for (idx in objTrue2) {
+    alert(typeof idx);
+}
+
+
+
