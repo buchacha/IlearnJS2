533df917883e51889dc10024547e9b3dac28291c
buchacha <egor322129@gmail.com>
2020-09-12 12:24:31 +0000

    forEach

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-let arr = [1, 2];
-
-let arrayLike = {
-  0: "что-то",
-  1: "ещё",
-  [Symbol.isConcatSpreadable]: true,
-  length: 2
-};
-
-alert( arr.concat(arrayLike) ); // 1,2,что-то,ещё
+["Bilbo", "Gandalf", "Nazgul"].forEach((item, index, array) => {
+  alert(`${item} имеет позицию ${index} в ${array}`);
+});
