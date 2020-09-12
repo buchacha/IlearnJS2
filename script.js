cc54c7254f045d22a56e5e042ac9df10fb0f742e
buchacha <egor322129@gmail.com>
2020-09-12 13:08:39 +0000

    inique

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+function unique(arr) {
+  let result = [];
 
-    // поменять элементы местами
-    // мы используем для этого синтаксис "деструктурирующее присваивание"
-    // подробнее о нём - в следующих главах
-    // то же самое можно записать как:
-    // let t = array[i]; array[i] = array[j]; array[j] = t
-    [array[i], array[j]] = [array[j], array[i]];
+  for (let str of arr) {
+    if (!result.includes(str)) {
+      result.push(str);
+    }
   }
+
+  return result;
 }
 
-let count = {
-  '123': 0,
-  '132': 0,
-  '213': 0,
-  '231': 0,
-  '321': 0,
-  '312': 0
-};
+let strings = ["кришна", "кришна", "харе", "харе",
+  "харе", "харе", "кришна", "кришна", ":-O"
+];
 
-for (let i = 0; i < 1000000; i++) {
-  let array = [1, 2, 3];
-  shuffle(array);
-  count[array.join('')]++;
-}
-
-// показать количество всех возможных вариантов
-for (let key in count) {
-  alert(`${key}: ${count[key]}`);
-}
+alert( unique(strings) ); // кришна, харе, :-O
