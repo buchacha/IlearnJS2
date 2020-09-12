e3f29a780951db045b84d19455079a9f57f2d64f
buchacha <egor322129@gmail.com>
2020-09-12 13:05:24 +0000

    custom shuffle (better)

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 function shuffle(array) {
-  array.sort(() => Math.random() - 0.5);
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+
+    // поменять элементы местами
+    // мы используем для этого синтаксис "деструктурирующее присваивание"
+    // подробнее о нём - в следующих главах
+    // то же самое можно записать как:
+    // let t = array[i]; array[i] = array[j]; array[j] = t
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
 
-let arr = [1, 2, 3];
-shuffle(arr);
-alert(arr);
+let count = {
+  '123': 0,
+  '132': 0,
+  '213': 0,
+  '231': 0,
+  '321': 0,
+  '312': 0
+};
+
+for (let i = 0; i < 1000000; i++) {
+  let array = [1, 2, 3];
+  shuffle(array);
+  count[array.join('')]++;
+}
+
+// показать количество всех возможных вариантов
+for (let key in count) {
+  alert(`${key}: ${count[key]}`);
+}
