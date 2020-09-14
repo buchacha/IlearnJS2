528cc58dcc00d3a81b1b7efaea88a6fa3da7bf31
buchacha <egor322129@gmail.com>
2020-09-14 19:34:23 +0000

    JSON parse with callback

--- script.js
+++ script.js
@@ -1,41 +1,8 @@
-function formatDate(date) {
-  let diff = new Date() - date; // разница в миллисекундах
+let str = '{"title":"Conference","date":"2017-11-30T12:00:00.000Z"}';
 
-  if (diff < 1000) { // меньше 1 секунды
-    return 'прямо сейчас';
-  }
+let meetup = JSON.parse(str, function(key, value) {
+  if (key == 'date') return new Date(value);
+  return value;
+});
 
-  let sec = Math.floor(diff / 1000); // преобразовать разницу в секунды
-
-  if (sec < 60) {
-    return sec + ' сек. назад';
-  }
-
-  let min = Math.floor(diff / 60000); // преобразовать разницу в минуты
-  if (min < 60) {
-    return min + ' мин. назад';
-  }
-
-  // отформатировать дату
-  // добавить ведущие нули к единственной цифре дню/месяцу/часам/минутам
-  let d = date;
-  d = [
-    '0' + d.getDate(),
-    '0' + (d.getMonth() + 1),
-    '' + d.getFullYear(),
-    '0' + d.getHours(),
-    '0' + d.getMinutes()
-  ].map(component => component.slice(-2)); // взять последние 2 цифры из каждой компоненты
-
-  // соединить компоненты в дату
-  return d.slice(0, 3).join('.') + ' ' + d.slice(3).join(':');
-}
-
-alert( formatDate(new Date(new Date() - 1)) ); // "прямо сейчас"
-
-alert( formatDate(new Date(new Date() - 30 * 1000)) ); // "30 сек. назад"
-
-alert( formatDate(new Date(new Date() - 5 * 60 * 1000)) ); // "5 мин. назад"
-
-// вчерашняя дата вроде 31.12.2016, 20:00
-alert( formatDate(new Date(new Date() - 86400 * 1000)) );
+alert( meetup.date.getDate() ); // 30 - теперь работает!
