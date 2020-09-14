d21abab28a9d2ae8111d21f786b71ac06d008c22
buchacha <egor322129@gmail.com>
2020-09-14 19:33:49 +0000

    Date object

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-'use strict'
+function formatDate(date) {
+  let diff = new Date() - date; // разница в миллисекундах
 
-let dinner = {buckwheat: 1, sausage:2};
+  if (diff < 1000) { // меньше 1 секунды
+    return 'прямо сейчас';
+  }
 
-let recipeMap = new Map([
-  [dinner, 500],
-  ["помидор", 350],
-  ["лук",    50]
-]);
+  let sec = Math.floor(diff / 1000); // преобразовать разницу в секунды
 
-alert(typeof recipeMap);
+  if (sec < 60) {
+    return sec + ' сек. назад';
+  }
 
-let obj = Object.fromEntries(recipeMap);
+  let min = Math.floor(diff / 60000); // преобразовать разницу в минуты
+  if (min < 60) {
+    return min + ' мин. назад';
+  }
 
-alert(typeof obj);
+  // отформатировать дату
+  // добавить ведущие нули к единственной цифре дню/месяцу/часам/минутам
+  let d = date;
+  d = [
+    '0' + d.getDate(),
+    '0' + (d.getMonth() + 1),
+    '' + d.getFullYear(),
+    '0' + d.getHours(),
+    '0' + d.getMinutes()
+  ].map(component => component.slice(-2)); // взять последние 2 цифры из каждой компоненты
 
-objTrue = {param:200};
-
-alert(typeof objTrue);
-
-for (elem in obj) {
-    alert(obj[elem]);
+  // соединить компоненты в дату
+  return d.slice(0, 3).join('.') + ' ' + d.slice(3).join(':');
 }
 
-let objTrue2 = {
-    param: 300,
-    {buckwheat:1, sausage:2}: 125 //unexpected token
-};
+alert( formatDate(new Date(new Date() - 1)) ); // "прямо сейчас"
 
-for (idx in objTrue2) {
-    alert(typeof idx);
-}
+alert( formatDate(new Date(new Date() - 30 * 1000)) ); // "30 сек. назад"
 
+alert( formatDate(new Date(new Date() - 5 * 60 * 1000)) ); // "5 мин. назад"
 
-
+// вчерашняя дата вроде 31.12.2016, 20:00
+alert( formatDate(new Date(new Date() - 86400 * 1000)) );
